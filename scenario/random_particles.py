c7a0b4f38ba17857116c9c0d820af0a597e26166
zenz <julian.zenz@hhu.de>
2019-09-23 16:30:58 +0200

    updated random_particles scenario and created one for 100

--- scenario/random_particles.py
+++ scenario/random_particles.py
@@ -1,7 +1,7 @@
 import csv
 import random
 
-from lib.std_lib import red, blue, black
+from lib.std_lib import red, blue, green
 
 
 def scenario(sim):
@@ -9,36 +9,36 @@ def scenario(sim):
     x_min, x_max = -sim.get_sim_x_size(), sim.get_sim_x_size()
     y_min, y_max = -sim.get_sim_y_size(), sim.get_sim_y_size()
 
-    particle_count = 400
+    particle_count = 100
 
     csv_file = open(sim.directory + '/particle_coords.csv', 'w', newline='')
     writer = csv.writer(csv_file)
     writer.writerow(["x", "y"])
 
     scenario_file = open(sim.directory + '/random_{}.py'.format(particle_count), 'w', newline='\n')
-    scenario_file.writelines(['from lib.std_lib import red, blue, black\n', '\n\n', 'def scenario(sim):\n'])
+    scenario_file.writelines(['from lib.std_lib import red, blue, green\n', '\n\n', 'def scenario(sim):\n'])
 
     # add borders
     scenario_file.writelines(['    # add borders as tiles\n'])
-    for x in range(x_min - 2, x_max + 2):
+    for x in range(round(x_min - 2), round(x_max + 2)):
         for y in [y_min - 2, y_max + 2]:
             sim.add_tile(x, y, red)
             scenario_file.writelines(['    sim.add_tile({}, {}, red)\n'.format(x, y)])
             if x != y:
                 sim.add_tile(y, x, blue)
-                scenario_file.writelines(['    sim.add_tile({}, {}, blue)\n'.format(x, y)])
+                scenario_file.writelines(['    sim.add_tile({}, {}, blue)\n'.format(y, x)])
 
     coords = []
     for _ in range(particle_count):
         x, y = random.randint(x_min, x_max), random.randrange(y_min, y_max, 2)
         while [x, y] in coords:
             x, y = random.randint(x_min, x_max), random.randrange(y_min, y_max, 2)
-        sim.add_particle(x, y, black)
+        sim.add_particle(x, y, green)
 
         coords.append([x, y])
 
         writer.writerow([x, y])
-        scenario_file.writelines(['    sim.add_particle({}, {}, black)\n'.format(x, y)])
+        scenario_file.writelines(['    sim.add_particle({}, {}, green)\n'.format(x, y)])
 
     csv_file.close()
     scenario_file.close()
