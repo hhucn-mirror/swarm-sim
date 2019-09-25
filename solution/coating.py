b2aa63e5d38bf355fbd4c5b51c50e58caaddc75d
Ahmad Reza <cheraghi@cs.uni-duesseldorf.de>
2019-09-25 12:17:54 +0200

    Merged with actual master branch

--- solution/coating.py
+++ solution/coating.py
@@ -23,43 +23,43 @@ def solution(sim):
                 print ("got a distance from a tile")
 
 
-        # if particle.wait:
-        #     if sim.get_actual_round() % (cycle_no + 1) == 0:
-        #         particle.wait = False
-        #     else:
-        #         #particle.delete_whole_memeory()
-        #         continue
-        # if sim.get_actual_round() % cycle_no == 1:
-        #     if particle.next_dir is not False and not particle.particle_in(particle.next_dir)\
-        #             and not particle.tile_in(particle.next_dir):
-        #         particle.prev_dir = get_the_invert(particle.next_dir)
-        #         particle.move_to(particle.next_dir)
-        #         print ("dist list bevore moving", str(particle.nh_dist_list)[1:-1] )
-        #         print("\n P", particle.number, " coates to", dir_to_str(particle.next_dir))
-        #         reset_attributes(particle)
-        #     elif move_to_dest_step_by_step(particle, particle.dest_t):
-        #         reset_attributes(particle)
-        #
-        # elif sim.get_actual_round() % cycle_no == 2:
-        #     read_data(particle)
-        #     particle.nh_dist_list = def_distances(particle)
-        #
-        #     particle.rcv_buf.clear()
-        #
-        # elif sim.get_actual_round() % cycle_no == 0:
-        #     particle.next_dir = coating_alg(particle)
-        #     if particle.next_dir is False:
-        #         if particle.number is not particle.p_max.id and particle.p_max.id is not None:
-        #             particle.p_max_table.update({particle.p_max.id: particle.p_max.dist})
-        #             if send_data(particle):
-        #                 continue
-        #
-        #     send_own_distance(particle)
-        #
-        #
-        #     #     #reset_attributes(particle)
-        #         #particle.wait = True
-        #     #     #data_clearing(particle)
-        #     # # else:
-        #     # #     move_to_dest_step_by_step(particle, particle.dest_t)
-        #     # #
+        if particle.wait:
+            if sim.get_actual_round() % (cycle_no + 1) == 0:
+                particle.wait = False
+            else:
+                #particle.delete_whole_memeory()
+                continue
+        if sim.get_actual_round() % cycle_no == 1:
+            if particle.next_dir is not False and not particle.particle_in(particle.next_dir)\
+                    and not particle.tile_in(particle.next_dir):
+                particle.prev_dir = get_the_invert(particle.next_dir)
+                particle.move_to(particle.next_dir)
+                print ("dist list bevore moving", str(particle.nh_dist_list)[1:-1] )
+                print("\n P", particle.number, " coates to", dir_to_str(particle.next_dir))
+                reset_attributes(particle)
+            elif move_to_dest_step_by_step(particle, particle.dest_t):
+                reset_attributes(particle)
+
+        elif sim.get_actual_round() % cycle_no == 2:
+            read_data(particle)
+            particle.nh_dist_list = def_distances(particle)
+
+            particle.rcv_buf.clear()
+
+        elif sim.get_actual_round() % cycle_no == 0:
+            particle.next_dir = coating_alg(particle)
+            if particle.next_dir is False:
+                if particle.number is not particle.p_max.id and particle.p_max.id is not None:
+                    particle.p_max_table.update({particle.p_max.id: particle.p_max.dist})
+                    if send_data(particle):
+                        continue
+
+            send_own_distance(particle)
+
+
+            #     #reset_attributes(particle)
+                #particle.wait = True
+            #     #data_clearing(particle)
+            # # else:
+            # #     move_to_dest_step_by_step(particle, particle.dest_t)
+            # #
