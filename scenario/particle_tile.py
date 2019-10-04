85c391dfbc76ee0395452e1bef508bbb8f931562
Gorden Wunderlich <Gorden.Wunderlich@uni-duesseldorf.de>
2019-10-04 14:49:41 +0200

    added scenario with one tile and many  randomly spread particles

---
+++ scenario/particle_tile.py
@@ -0,0 +1,52 @@
+from lib.swarm_sim_header import *
+
+max = 40
+def scenario(sim):
+    # sim.add_tile(0.0, 2.0)
+    # sim.add_tile(3.0, 2.0)
+    # sim.add_tile(0.5, 3.0)
+    # sim.add_tile(1.5, 3.0)
+    # sim.add_tile(2.5, 3.0)
+    # sim.add_tile(3.5, 1.0)
+    # sim.add_tile(3.0, 0.0)
+    # sim.add_tile(0.0, 0.0)
+    # sim.add_tile(-0.5, 1.0)
+    # sim.add_tile(0.0, 2.0)
+    # sim.add_tile(3.0, 2.0)
+    # sim.add_tile(0.5, 3.0)
+    # sim.add_tile(1.5, 3.0)
+    # sim.add_tile(2.5, 3.0)
+    # sim.add_tile(3.5, 1.0)
+    # sim.add_tile(3.0, 0.0)
+    sim.add_tile(0.0, 0.0)
+    # sim.add_tile(-0.5, 1.0)
+
+
+    generating_random_spraded_particles(sim, max)
+
+    # sim.add_particle(1.0, 0.0)
+    # sim.add_particle(1.5, 1.0)
+    # sim.add_particle(2.0, 0.0)
+    # sim.add_particle(2.5, 1.0)
+    # sim.add_particle(3.0, -0.0)
+    # sim.add_particle(3.5, 1.0)
+    # sim.add_particle  (10.0, 20.0)
+    # sim.add_particle(-10.0, 20.0)
+    # sim.add_particle(-10.0, -20.0)
+    # sim.add_particle(10.0, -20.0)
+    #
+    # sim.add_particle(10, 0.0)
+    #
+    # sim.add_particle(-10, 0.0)
+    #
+    # sim.add_particle(0, -20.0)
+    # sim.add_particle(0, 20.0)
+
+    # sim.add_particle(1.5, 1.0)
+    # sim.add_particle(0.5, 1.0)
+    # sim.add_particle(1.0, -0.0)
+    # sim.add_particle(0.5, -1.0)
+    # sim.add_particle(1.5, -1.0)
+    # sim.add_particle(2.0, 0.0)
+    # sim.add_particle(2.5, 1.0)
+    # sim.add_particle(2.5, -1.0)
