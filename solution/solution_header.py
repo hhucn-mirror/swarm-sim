5840cbd8d0230f97d2ee2ee97d88a3d820214539
Gorden Wunderlich <gorden.wunderlich@hhu.de>
2019-11-07 16:07:44 +0100

    added documentation comments to solution_header

--- solution/solution_header.py
+++ solution/solution_header.py
@@ -4,6 +4,11 @@ from copy import deepcopy
 
 class OwnDistance:
     def __init__(self, particle_distance, particle_id):
+        """
+        A message package, which contains information about the distance of the sender to the next tile.
+        :param particle_distance: distance of the sender to the next tile
+        :param particle_id: id of the sender particle
+        """
         self.particle_distance = particle_distance
         self.particle_id = particle_id
 
@@ -13,6 +18,14 @@ class OwnDistance:
 
 class PMax(OwnDistance):
     def __init__(self, particle_distance, particle_id, p_max, p_max_table):
+        """
+        In addition to the information in OwnDistance this package contains information about the maximum particle distance
+        known to this particle.
+        :param particle_distance: distance of the sender to the next tile
+        :param particle_id: id of the sender particle
+        :param p_max: maximum distance of a particle known to this particle
+        :param p_max_table: currently unused
+        """
         OwnDistance.__init__(self, particle_distance, particle_id)
         self.p_max_ids = p_max.ids
         self.p_max_dist = p_max.dist
@@ -26,6 +39,11 @@ class PMax(OwnDistance):
 
 class Neighbor:
     def __init__(self, type, dist):
+        """
+        A type storing the type and calculated distance for a neighbor of a particle
+        :param type: type of the neighbor (fl, p, t)
+        :param dist: calculated distance of the neighbor to the next tile
+        """
         self.type = type
         self.dist = dist
 
@@ -35,6 +53,13 @@ class Neighbor:
 
 class PMaxInfo:
     def __init__(self):
+        """
+        A type for organizing information about the current p_max.
+        ids: all particle ids that are believed to have the maximum distance
+        dist: maximum particle distance known to this particle
+        directions: all directions from where this particle received messages with the current PMax
+        black_list: unused
+        """
         self.ids = set()
         self.dist = -math.inf
         self.directions = []
