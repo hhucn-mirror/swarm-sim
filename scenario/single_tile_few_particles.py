33eb3c886d99b39f6f394c2042c07afb1cb6dd23
Gorden Wunderlich <gorden.wunderlich@hhu.de>
2019-11-20 15:09:51 +0100

    created scenarios

---
+++ scenario/single_tile_few_particles.py
@@ -0,0 +1,6 @@
+from lib.swarm_sim_header import *
+
+max = 20
+def scenario(sim):
+    sim.add_tile(0.0, 0.0)
+    generating_random_spraded_particles(sim, max)
--- scenario/single_tile_particle_line.py
+++ scenario/single_tile_particle_line.py
@@ -14,10 +14,15 @@ def scenario(sim):
     sim.add_tile(0, 0)
 
     sim.add_particle(0.5, 1.0)
-    sim.add_particle (1.0, 0.0)
-    sim.add_particle  (2.0, 0.0)
-    sim.add_particle  (3.0, 0.0)
-    sim.add_particle  (4.0, 0.0)
+    sim.add_particle(0.5, -1.0)
+    sim.add_particle(1.0, 0.0)
+    sim.add_particle(2.0, 0.0)
+    sim.add_particle(3.0, 0.0)
+    sim.add_particle(4.0, 0.0)
+    sim.add_particle(5.0, 0.0)
+    sim.add_particle(6.0, 0.0)
+    sim.add_particle(7.0, 0.0)
+
 
     # sim.add_particle (1.0, 0.0)
     # sim.add_particle  (1.5, 1.0)
