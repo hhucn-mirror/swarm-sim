e45cb56974702d47414c095c3574342ef1f2cf89
Gorden Wunderlich <gorden.wunderlich@hhu.de>
2019-12-05 16:42:17 +0100

    added termination to all solutions

--- solution/only_move_if_best_match/main.py
+++ solution/only_move_if_best_match/main.py
@@ -34,6 +34,8 @@ def solution(sim):
 
         elif sim.get_actual_round() % cycle_no == 0:
             write_cycle(particle)
+    if goal_reached(sim):
+        sim.set_successful_end()
 
 
 def write_cycle(particle):
@@ -113,3 +115,17 @@ def move_to_target_tile(particle):
     else:
         coating_mod.reset_attributes(particle)
         coating_mod.reset_p_max(particle)
+
+
+def goal_reached(sim):
+    min_fl_distance = min(list(map(get_smallest_fl, sim.particles)), default=0)
+    max_particle_distance = max([particle.own_dist for particle in sim.particles], default=math.inf)
+    if min_fl_distance is math.inf or min_fl_distance < max_particle_distance:
+        return False
+    else:
+        return True
+
+
+def get_smallest_fl(particle):
+    return min([neighbor.dist for neighbor in particle.nh_list if neighbor.type == "fl"], default=math.inf)
+
