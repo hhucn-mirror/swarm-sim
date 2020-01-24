05a2e518324e22c95fea3d386b48b83a866e97ba
Gorden Wunderlich <gorden.wunderlich@hhu.de>
2020-01-24 17:19:00 +0100

    now prints successful message and rounds taken if the simulation completes successfully

--- solution/goal_test.py
+++ solution/goal_test.py
@@ -4,6 +4,8 @@ import math
 def end_sim(sim):
     if goal_reached(sim):
         sim.set_successful_end()
+        if sim.config_data.visualization:
+            print("successful end reached after round:", sim.get_actual_round())
 
 
 def goal_reached(sim):
